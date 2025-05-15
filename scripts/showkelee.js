4a6de90b3a0cb0ad92280a7b6584e977b48a93ba
鑫鹏 <34210723+canff@users.noreply.github.com>
2025-05-16 07:24:43 +0800

    Update showkelee.js

--- scripts/showkelee.js
+++ scripts/showkelee.js
@@ -1,12 +1,13 @@
+// Loon Surge compatible
 if ($response && $response.headers) {
   const headers = $response.headers;
 
-  // 移除触发下载的字段
+  // 删除 Content-Disposition，避免下载
   if (headers['Content-Disposition']) {
     delete headers['Content-Disposition'];
   }
 
-  // 设置为纯文本
+  // 设置文本显示类型
   headers['Content-Type'] = 'text/plain; charset=utf-8';
 
   $done({ headers });
